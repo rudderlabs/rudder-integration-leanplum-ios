8506722e710d8b125e5027bea40326ad16a1a1ae
arnab <arnab@rudderlabs.com>
2020-03-04 17:25:20 +0530

    Updated screen calls and value properties

--- Rudder-Leanplum/Classes/RudderLeanplumIntegration.m
+++ Rudder-Leanplum/Classes/RudderLeanplumIntegration.m
@@ -35,11 +35,16 @@
             
             NSMutableDictionary *traits = [client getContext].traits;
             NSString *userId = traits[@"userId"];
-            if (userId != nil) {
-                [Leanplum startWithUserId:userId];
-            } else {
-                [Leanplum start];
+            if (userId == nil) {
+                userId = traits[@"id"];
             }
+            dispatch_async(dispatch_get_main_queue(), ^ {
+                if (userId != nil) {
+                    [Leanplum startWithUserId:userId];
+                } else {
+                    [Leanplum start];
+                }
+            });
         }
         
     }
@@ -48,8 +53,14 @@
 }
 
 - (void) dump:(RudderMessage *)message {
-    if (message != nil) {
-        [self processRudderEvent:message];
+    @try {
+        if (message != nil) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+              [self processRudderEvent:message];
+            });
+        }
+    } @catch (NSException *ex) {
+        [RudderLogger logError:[[NSString alloc] initWithFormat:@"%@", ex]];
     }
 }
 
@@ -57,9 +68,24 @@
     NSString *type = message.type;
     if ([type isEqualToString:@"identify"]) {
         [Leanplum setUserId:message.userId withUserAttributes:message.context.traits];
-    } else if ([type isEqualToString:@"track"] || [type isEqualToString:@"screen"]) {
+    } else if ([type isEqualToString:@"track"]) {
         if (message.event != nil) {
-            [Leanplum track:message.event withParameters:message.properties];
+            NSDictionary *properties = message.properties;
+            id value = [properties objectForKey:@"value"];
+            if (value != nil) {
+                if ([value isKindOfClass:[NSNumber class]]) {
+                    double val = [value doubleValue];
+                    [Leanplum track:message.event withValue:val andParameters:properties];
+                } else {
+                    [Leanplum track:message.event withParameters:message.properties];
+                }
+            } else {
+                [Leanplum track:message.event withParameters:message.properties];
+            }
+        }
+    } else if ([type isEqualToString:@"screen"]) {
+        if (message.event != nil) {
+            [Leanplum advanceTo:message.event withParameters:message.properties];
         }
     }
 }
